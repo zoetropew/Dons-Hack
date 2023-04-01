89c70591011de86fbe4c82958a5fc20706bd2136
zoetropew <zawong2@dons.usfca.edu>
2023-04-01 00:53:28 -0700

    adding text in image recognition

---
+++ donsHack/backend/scanner.mjs
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+// const axios = require("axios");
+
+const options = {
+  method: 'POST',
+  url: 'https://text-in-images-recognition.p.rapidapi.com/prod',
+  headers: {
+    'content-type': 'application/json',
+    'X-RapidAPI-Key': '8a747041b9mshc6017c4b82c1f35p1735cbjsn8c26a1d30b32',
+    'X-RapidAPI-Host': 'text-in-images-recognition.p.rapidapi.com'
+  },
+  data: '{"objectUrl":"https://miro.medium.com/max/2400/1*T8LN_mDq8vNrD63IIIgzjQ.png"}'
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
+console.log("Node file is running and working");
+
+export function forButton() {
+    return "this still works ahhahahah";
+}
