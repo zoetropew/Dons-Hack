ad362cae041a63367fe616eba46e9652a9cda7dd
t1fffany <102014820+t1fffany@users.noreply.github.com>
2023-04-01 10:26:04 -0700

    Update two.tsx

--- donsHack/app/(tabs)/two.tsx
+++ donsHack/app/(tabs)/two.tsx
@@ -7,6 +7,7 @@ export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
+      <Text style ={styles.box}> Let's get started</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
@@ -18,6 +19,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  box: {
+    color: 'white',
+    fontSize: 30,
+    marginLeft: -15,
+    width: 300,
+    height: 135,
+    backgroundColor: '#8783dd',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
   },
   title: {
     fontSize: 20,
