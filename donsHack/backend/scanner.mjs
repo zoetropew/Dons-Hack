d8e622a0080c045de4da0b40b315272929314a10
zoetropew <zawong2@dons.usfca.edu>
2023-04-01 03:59:37 -0700

    storing last api call in file for test purposes

--- donsHack/backend/scanner.mjs
+++ donsHack/backend/scanner.mjs
@@ -1,24 +1,34 @@
 import axios from 'axios';
+import fs from 'fs';
+import TreeMap from 'treemap-js';
 
-const options = {
-  method: 'POST',
-  url: 'https://text-in-images-recognition.p.rapidapi.com/prod',
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': '8a747041b9mshc6017c4b82c1f35p1735cbjsn8c26a1d30b32',
-    'X-RapidAPI-Host': 'text-in-images-recognition.p.rapidapi.com'
-  },
-  // Example
-  // data: '{"objectUrl":"https://miro.medium.com/max/2400/1*T8LN_mDq8vNrD63IIIgzjQ.png"}'
+var map = new TreeMap();
+
+// DO NOT UNCOMMENT: I only have 100 free API calls and I already used 30 of them lol
+// to access test data read from whiteboard.jpeg, use output.json which was generated from the last API call
+
+// const options = {
+//   method: 'POST',
+//   url: 'https://text-in-images-recognition.p.rapidapi.com/prod',
+//   headers: {
+//     'content-type': 'application/json',
+//     'X-RapidAPI-Key': '8a747041b9mshc6017c4b82c1f35p1735cbjsn8c26a1d30b32',
+//     'X-RapidAPI-Host': 'text-in-images-recognition.p.rapidapi.com'
+//   },
+//   // Example
+//   // data: '{"objectUrl":"https://miro.medium.com/max/2400/1*T8LN_mDq8vNrD63IIIgzjQ.png"}'
   
-  // Testing handwriting source data
-  data: '{"objectUrl":"https://raw.githubusercontent.com/zoetropew/Dons-Hack/main/donsHack/backend/testData/whiteboard.jpeg"}'
-};
+//   // Testing handwriting source data
+//   data: '{"objectUrl":"https://raw.githubusercontent.com/zoetropew/Dons-Hack/main/donsHack/backend/testData/whiteboard.jpeg"}'
+// };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+// axios.request(options).then(function (response) {
+//   fs.writeFile('output.json', JSON.stringify(response.data), function (err) {
+//     if (err) throw err;
+//     console.log('Response data saved to output.json');
+//   });
+// }).catch(function (error) {
+// 	console.error(error);
+// });
 
 console.log("Node file is running and working");
