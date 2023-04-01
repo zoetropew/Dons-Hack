7412c980aac2619eabcb68ed438514878d21f981
zoetropew <zawong2@dons.usfca.edu>
2023-04-01 11:54:24 -0700

    index data structure

--- donsHack/backend/storeNote.mjs
+++ donsHack/backend/storeNote.mjs
@@ -6,19 +6,41 @@ import TreeMap from 'treemap-js';
 var map = new TreeMap();
 var note = "";
 
-fs.readFile('./output.json', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    
-    const jsonData = JSON.parse(data);
-    jsonData.TextDetections.forEach((obj) => {
-      if (obj.Type == "LINE") {
-        note += obj.DetectedText + "\n";
-      }
-    });
-    console.log(note);
-  });
+export function readNote(title) {
+    fs.readFile('./output.json', (err, data) => {
+        if (err) {
+        console.error(err);
+        return;
+        }
+        
+        const jsonData = JSON.parse(data);
+        jsonData.TextDetections.forEach((obj) => {
+        if (obj.Type == "LINE") {
+            note += obj.DetectedText + "\n";
+        }
+          else if (obj.Type == "WORD") {
+            let temp = map.get(title);
+            if (temp == null) {
+                temp = new Set();
+            }
+            temp.add(obj.DetectedText);
+            map.set(title, temp);
+          }
+        });
+        console.log(note);
 
-  
+        console.log("printing map...");
+        map.each(function (value, key) {
+          console.log("Key: " + key);
+          console.log("value: ");
+
+          for(var item of value.values()){
+            console.log(item);
+          }
+        })
+
+        return note;
+    });
+}
+
+readNote("something");
