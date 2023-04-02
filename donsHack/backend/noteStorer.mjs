cfa35b1420edc96d8525fcddbaf045064495b557
zoetropew <zawong2@dons.usfca.edu>
2023-04-02 05:10:37 -0700

    increased note reading capabilities

--- donsHack/backend/noteStorer.mjs
+++ donsHack/backend/noteStorer.mjs
@@ -2,7 +2,7 @@
 
 
 export function readNote(title, data) {
-    var map = new Map();
+    // var map = new Map();
     var note = "";
 
     // console.log(typeof(data));
@@ -11,17 +11,17 @@ export function readNote(title, data) {
     if (obj.Type == "LINE") {
         note += obj.DetectedText + "\n";
     }
-      else if (obj.Type == "WORD") {
-        let temp = map.get(title);
-        if (temp == null) {
-            temp = new Set();
-        }
-        let string = obj.DetectedText.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-        temp.add(string);
-        map.set(title, temp);
-      }
+      // else if (obj.Type == "WORD") {
+      //   let temp = map.get(title);
+      //   if (temp == null) {
+      //       temp = new Set();
+      //   }
+      //   let string = obj.DetectedText.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+      //   temp.add(string);
+      //   map.set(title, temp);
+      // }
     });
-    // console.log(note);
+    console.log(note);
     // console.log("printing map...");
     // map.forEach(function (value, key) {
     //   console.log("Key: " + key);
@@ -32,4 +32,42 @@ export function readNote(title, data) {
     //   }
     // })
     return note;
+}
+
+export function readNoteMap(title, data) {
+  var map = new Map();
+  const jsonData = JSON.parse(data);
+  jsonData.TextDetections.forEach((obj) => {
+    if (obj.Type == "WORD") {
+      let temp = map.get(title);
+      if (temp == null) {
+        temp = new Set();
+      }
+      let string = obj.DetectedText.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+      temp.add(string);
+      map.set(title, temp);
+    }
+  });
+  console.log("printing map...");
+  map.forEach(function (value, key) {
+    console.log("Key: " + key);
+    console.log("value: ");
+
+    for(var item of value.values()){
+      console.log(item);
+    }
+  });
+  return map;
+}
+
+export function readNoteSet(data) {
+  var set = new Set();
+  const jsonData = JSON.parse(data);
+  jsonData.TextDetections.forEach((obj) => {
+    if (obj.Type == "WORD") {
+      let string = obj.DetectedText.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+      set.add(string);
+    }
+  });
+  return set;
 }
