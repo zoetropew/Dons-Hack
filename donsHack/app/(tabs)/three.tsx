9ccc7511dbb48da20106556212f49730a4d5b638
zoetropew <zawong2@dons.usfca.edu>
2023-04-02 01:49:22 -0700

    notes list as alerts

--- donsHack/app/(tabs)/three.tsx
+++ donsHack/app/(tabs)/three.tsx
@@ -1,13 +1,36 @@
-import { StyleSheet, Button } from 'react-native';
+import { StyleSheet, Button, Alert } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+
+import { readNote } from '../../backend/noteStorer.mjs';
 
 import noteList from '../../backend/noteList.mjs';
 
 // export default function TabThreeScreen(notes: noteList, results: noteList) {
   export default function NotesScreen() {
   let view = "Documents";
+
+  const jsonData1 = require('../../backend/astrophysics.json');
+  let text1 = JSON.stringify(jsonData1);
+  let astrophysics = readNote("4/21 Note", text1);
+
+  const jsonData2 = require('../../backend/cs.json');
+  let text2 = JSON.stringify(jsonData2);
+  let cs = readNote("3/21 Note", text2);
+
+  const jsonData3 = require('../../backend/energy.json');
+  let text3 = JSON.stringify(jsonData3);
+  let energy = readNote("2/21 Note", text3);
+
+  const jsonData4 = require('../../backend/ds.json');
+  let text4 = JSON.stringify(jsonData4);
+  let ds = readNote("1/21 Note", text4);
+
+  const jsonData5 = require('../../backend/relativity.json');
+  let text5 = JSON.stringify(jsonData5);
+  let relativity = readNote("12/21 Note", text5);
+
   return (
     <View style={styles.container}>
       <View style={styles.tbox}>
@@ -16,22 +39,37 @@ import noteList from '../../backend/noteList.mjs';
     <View style={styles.cbox}>
     <Button
     title = "4/21 Note"
-    color="black"/>
+    color="black"
+    onPress={() => Alert.alert(astrophysics)}
+    />
     </View>  
     <View style={styles.cbox}>
     <Button
     title = "3/21 Note"
-    color="black"/>
+    color="black"
+    onPress={() => Alert.alert(cs)}
+    />
     </View> 
     <View style={styles.cbox}>
     <Button
     title = "2/21 Note"
-    color="black"/>
+    color="black"
+    onPress={() => Alert.alert(energy)}
+    />
     </View> 
     <View style={styles.cbox}>
     <Button
     title = "1/21 Note"
-    color="black"/>
+    color="black"
+    onPress={() => Alert.alert(ds)}
+    />
+    </View> 
+    <View style={styles.cbox}>
+    <Button
+    title = "12/21 Note"
+    color="black"
+    onPress={() => Alert.alert(relativity)}
+    />
     </View> 
     </View>
   );
