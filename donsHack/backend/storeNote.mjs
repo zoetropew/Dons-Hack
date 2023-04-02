56c8fc7224b2d36bb9fea5cca12dfe4dffc2b499
zoetropew <zawong2@dons.usfca.edu>
2023-04-01 18:50:56 -0700

    storeNote that is able to work with frontend

--- donsHack/backend/storeNote.mjs
+++ donsHack/backend/storeNote.mjs
@@ -1,46 +1,38 @@
 // Reads output.json to store note and index
 
-import fs from 'fs';
-import TreeMap from 'treemap-js';
+export function readNote(title, data) {
+    var map = new Map();
+    var note = "";
 
-var map = new TreeMap();
-var note = "";
-
-export function readNote(title) {
-    fs.readFile('./output.json', (err, data) => {
-        if (err) {
-        console.error(err);
-        return;
+    console.log(typeof(data));
+    const jsonData = JSON.parse(data);
+    jsonData.TextDetections.forEach((obj) => {
+    if (obj.Type == "LINE") {
+        note += obj.DetectedText + "\n";
+    }
+      else if (obj.Type == "WORD") {
+        let temp = map.get(title);
+        if (temp == null) {
+            temp = new Set();
         }
-        
-        const jsonData = JSON.parse(data);
-        jsonData.TextDetections.forEach((obj) => {
-        if (obj.Type == "LINE") {
-            note += obj.DetectedText + "\n";
-        }
-          else if (obj.Type == "WORD") {
-            let temp = map.get(title);
-            if (temp == null) {
-                temp = new Set();
-            }
-            temp.add(obj.DetectedText);
-            map.set(title, temp);
-          }
-        });
-        console.log(note);
-
-        console.log("printing map...");
-        map.each(function (value, key) {
-          console.log("Key: " + key);
-          console.log("value: ");
-
-          for(var item of value.values()){
-            console.log(item);
-          }
-        })
-
-        return note;
+        let string = obj.DetectedText.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+        temp.add(string);
+        map.set(title, temp);
+      }
     });
+    console.log(note);
+
+    console.log("printing map...");
+    map.forEach(function (value, key) {
+      console.log("Key: " + key);
+      console.log("value: ");
+
+      for(var item of value.values()){
+        console.log(item);
+      }
+    })
+
+    return note;
 }
 
-readNote("something");
+// readNote("something");
