4ae495db5441d241ae140335090106e62f166c2a
zoetropew <zawong2@dons.usfca.edu>
2023-04-02 00:35:54 -0700

    creating text input for assignments

--- donsHack/app/(tabs)/four.tsx
+++ donsHack/app/(tabs)/four.tsx
@@ -1,22 +1,47 @@
-import { StyleSheet,Button } from 'react-native';
+import { StyleSheet,Button, TextInput } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import NotesScreen from './three';
+
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
-export default function TabOneScreen() {
+import React from 'react';
+
+export default function AssignmentScreen() {
+
+  const [textInputValue, setTextInputValue] = React.useState('');
+
+  const handleTextInputChange = (text: string) => {
+    setTextInputValue(text);
+  };
+
+  const handleSubmit = () => {
+    console.log(textInputValue);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.box}>
       <Text style={styles.text}>Upload</Text>
       <Text style={styles.text}>Assignment</Text>
       </View>
-      <View style={styles.textBox}> 
-        <Text style={styles.text}></Text>
+      <View style={styles.textBox}>
+        <TextInput
+          style={styles.input}
+          onChangeText={handleTextInputChange}
+          value={textInputValue}
+          placeholder='Paste your assignment here!'
+          placeholderTextColor="#b09bc9"
+        />
+        {/* <Text style={styles.text}></Text> */}
         </View>
       <View style={styles.sBox}>
-    <Button
-    title = "Submit"
-    color="white"/>
+      <Button 
+        title="Submit" 
+        onPress={handleSubmit} 
+        color="white"
+        />
     </View>  
     </View>
   );
@@ -56,6 +81,14 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     marginRight: 30,
     fontSize: 30,
+    fontWeight: 'normal',
+    color: 'black',
+  },
+  input: {
+    marginTop: 15,
+    marginLeft: 20,
+    marginRight: 30,
+    fontSize: 14,
     fontWeight: 'normal',
     color: 'black',
   },
