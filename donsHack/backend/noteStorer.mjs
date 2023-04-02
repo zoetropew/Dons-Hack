353079d8d344893d15cb71adc9794ba4c9962395
zoetropew <zawong2@dons.usfca.edu>
2023-04-02 05:19:31 -0700

    cleaning comments

--- donsHack/backend/noteStorer.mjs
+++ donsHack/backend/noteStorer.mjs
@@ -2,35 +2,14 @@
 
 
 export function readNote(title, data) {
-    // var map = new Map();
     var note = "";
-
-    // console.log(typeof(data));
     const jsonData = JSON.parse(data);
     jsonData.TextDetections.forEach((obj) => {
     if (obj.Type == "LINE") {
         note += obj.DetectedText + "\n";
     }
-      // else if (obj.Type == "WORD") {
-      //   let temp = map.get(title);
-      //   if (temp == null) {
-      //       temp = new Set();
-      //   }
-      //   let string = obj.DetectedText.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-      //   temp.add(string);
-      //   map.set(title, temp);
-      // }
     });
     console.log(note);
-    // console.log("printing map...");
-    // map.forEach(function (value, key) {
-    //   console.log("Key: " + key);
-    //   console.log("value: ");
-
-    //   for(var item of value.values()){
-    //     console.log(item);
-    //   }
-    // })
     return note;
 }
 
